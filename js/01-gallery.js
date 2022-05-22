a08c874e9da32d879a5c995533097616a6808f4a
NataliaDelicheban <nataliamansarliyska@gmail.com>
2022-05-22 21:53:40 +0200

    creat function

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const createGallery = (galleryItems) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>`;
+};
+const galleryBox = document.querySelector(".gallery")
+
+galleryBox.insertAdjacentHTML("beforeend", createGallery());
+console.log(galleryBox);
+
+
+//console.log(galleryItems);
